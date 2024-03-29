92dc3a7b88628682a786e14ebca9b50e9d007f4a
julioaraujo96 <julioaraujo.bs@gmail.com>
2024-03-29 18:01:13 +0000

    initial setup

---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
