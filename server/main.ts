4eeb1c428f47ef5477dd79a80f967342b2f970dd
julioaraujo96 <julioaraujo.bs@gmail.com>
2025-02-23 15:36:51 +0000

    feat(api): db schema

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import authRoutes from './src/routes/auth.routes';
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
