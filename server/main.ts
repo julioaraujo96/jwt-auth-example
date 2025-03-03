526bfb168a33e283182bcce3fca5c5745b82ecd8
julioaraujo96 <julioaraujo.bs@gmail.com>
2025-03-03 00:29:11 +0000

    feat(api): add cookie parser

--- server/main.ts
+++ server/main.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './src/routes';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use('/api', routes);
 
